46cca20c876f609d670e2bfce2c7e11da5c6d8d2
Miha258 <rizoks29@gmail.com>
2024-08-07 02:43:29 +0300

    m

--- main.py
+++ main.py
@@ -163,10 +163,11 @@ async def check_subscription():
                 for user in users:
                     is_in_chat = await bot.get_chat_member(chat, user.chat_id)
                     if is_in_chat:
-                        print(is_in_chat.status, is_in_chat.status == "kicked")
                         if is_in_chat.status not in ("kicked", "left", "banned"):
                             if not is_in_chat.is_chat_admin():
                                 await bot.kick_chat_member(chat, user.chat_id)
+                                user.subscription_active = False
+                                session.commit()    
                                 for admin in admins:
                                     try:
                                         tg_user = await bot.get_chat(user.chat_id)
